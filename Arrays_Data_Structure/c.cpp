f8f98fdd535b0a25fcd25150a945694fa9c07fbd
kelash <kelash.raisal@gmail.com>
2023-01-04 20:31:15 +0500

    searching in array

--- Arrays_Data_Structure/c.cpp
+++ Arrays_Data_Structure/c.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
+int number;
     int arr[]={2,3,4,4,55,6,6,7,7,8};
-    // int size =arr[0]/arr.size();
+    cout<<"Enter element to search in array ";
+    cin>>number;
     for(int i=0;i<10;i++){
+        if(arr[i]==number)
+            cout<<"your searched element is  at index "<<i;
         cout<<" "<<i;
     }
 
