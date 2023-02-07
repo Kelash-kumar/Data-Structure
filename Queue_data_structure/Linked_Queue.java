dd54d29bca3704f012fcfe30c64012244ba12042
kelash <kelash.raisal@gmail.com>
2023-02-07 20:40:48 +0500

    array using queue

--- Queue_data_structure/Linked_Queue.java
+++ Queue_data_structure/Linked_Queue.java
@@ -9,16 +9,47 @@ interface Queue{
   }
 
   class LinkedQueue implements Queue{
+      Node head;
+    int size;
+    
+    private  class Node{
+        Object objects;
+        Node next;
+        Node prev;
+        Node(Object onObjects){
+          this.objects=onObjects;
+        }
+
+        Node(Object obj,Node prev,Node next){
+            objects=obj;
+            this.prev=prev;
+            this.next=next;
+
+
+        }
+    }
 
     @Override
     public void push(Object obj) {
-        
+    head.prev= head.prev.next=new Node(obj);
+    size++;
+    
     }
 
     @Override
     public Object remove() {
+        if(isEmpty())throw new NullPointerException();
+
+
+        Object first=head.prev;
+        head=head.prev.next;
+        head.prev=head.prev.next.next;
+
+        size--;
         return null;
     }
+
+
 
     @Override
     public int size() {
@@ -30,7 +61,10 @@ interface Queue{
         return null;
     }
 
+    private boolean isEmpty(){return size==0;}
   }
+ 
+  
   
 public class Linked_Queue {
     public static void main(String[] args) {
