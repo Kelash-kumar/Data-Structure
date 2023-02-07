e23e35bc0e6e77fb4b7c0d9ab94482db5362a254
kelash <kelash.raisal@gmail.com>
2023-02-07 18:27:02 +0500

    queue data structure

---
+++ Queue_data_structure/Linked_Queue.java
@@ -0,0 +1,39 @@
+package Queue_data_structure;
+
+
+interface Queue{
+    void push(Object obj);
+    Object remove();
+      int size();
+      Object first();
+  }
+
+  class LinkedQueue implements Queue{
+
+    @Override
+    public void push(Object obj) {
+        
+    }
+
+    @Override
+    public Object remove() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object first() {
+        return null;
+    }
+
+  }
+  
+public class Linked_Queue {
+    public static void main(String[] args) {
+        
+    }
+}
