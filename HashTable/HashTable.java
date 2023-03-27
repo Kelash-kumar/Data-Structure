433e04c5d0eca29fe3796006155b676aa6bd5d3d
kelash <kelash.raisal@gmail.com>
2023-03-27 19:52:56 +0500

    some changes

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -25,22 +25,22 @@ class Hash_List {
     public void dump() {
 
         for (int i = 0; i < List.length; i++) {
-            
+
             System.out.print(i + ":");
-            
+
             ListNode list = List[i]; // For traversing linked list value i.
-            
+
             while (list != null) {
-            
-            System.out.print(" (" + list.key + "," + list.value + ")");
-            
-            list = list.next;
-            
+
+                System.out.print(" (" + list.key + "," + list.value + ")");
+
+                list = list.next;
+
             }
-            
+
             System.out.println();
-            
-            }
+
+        }
     }
 
     public void put(Object key, Object value) {
@@ -69,23 +69,21 @@ class Hash_List {
         }
     }
 
-public Object get(Object key) {
-    int bucket = hash(key);
-    ListNode list=List[bucket];  //for traversing the linked list at appropraiate positoin
+    public Object get(Object key) {
+        int bucket = hash(key);
+        ListNode list = List[bucket]; // for traversing the linked list at appropraiate positoin
 
-while(list!=null){
+        while (list != null) {
 
-    if(list.key.equals(key))
+            if (list.key.equals(key))
 
-        return list.value;
-    
-    
-    list=list.next;
-}
- 
-return null;
-}
+                return list.value;
 
+            list = list.next;
+        }
+
+        return null;
+    }
 
     public void remove(Object key) {
         int bucket = hash(key);
@@ -120,15 +118,14 @@ return null;
                 return true;
             list = list.next;
         }
-     
-    return false;   
+
+        return false;
     }
 
-private int hash(Object key){
+    private int hash(Object key) {
 
-    return (Math.abs(key.hashCode()%List.length));
-}
-
+        return (Math.abs(key.hashCode() % List.length));
+    }
 
     private void resize() {
         ListNode[] newList = new ListNode[List.length * 2];
@@ -163,110 +160,93 @@ private int hash(Object key){
     }
 }
 
- 
-
 public class HashTable {
     public static void main(String[] args) {
-      
-try (Scanner textIO = new Scanner(System.in)) {
-    Hash_List List = new Hash_List(5);
-    
-    Object key,value;
-    
-    while (true) {
-    
-    System.out.println("\n Menu:");
-    
-    System.out.println(" 1.  Add_into_hash(key,value)");
-    
-    System.out.println(" 2.  get_from_hash(key)");
-    
-    System.out.println(" 3.  Contains_in_hash(key)");
-    
-    System.out.println(" 4.  Remove_from_hash(key)");
-    
-    System.out.println(" 5. Show Complete Contents Of Hash List.");
 
-    System.out.println(" 6. Update_in_hash(key) ");
+        try (Scanner textIO = new Scanner(System.in)) {
+            Hash_List List = new Hash_List(5);
 
-    System.out.println(" 7. EXIT");
-    
-    System.out.print("Enter your command: ");
-    
-    switch (textIO.nextInt()) {
-    
-    case 1:
-    
-    System.out.print("\n key = ");
-    
-    key = textIO.next();
-    
-    System.out.print(" value = ");
-    
-    value = textIO.next();
-    
-    List.put(key,value);
-    
-    
-    break;
-    
-    case 2:
-    
-    System.out.print("\n key = ");
-    
-    key = textIO.next();
-    
-    System.out.println("The value at key "+key+"   is " + List.get(key));
-    
-    break;
-    
-    case 3:
-    
-    System.out.print("\n key = ");
-    
-    key = textIO.next();
-    
-    System.out.println(" containsKey(" + key + ") is "+ List.containsKey(key));
-    
-    break;
-    
-    case 4:
-    
-    System.out.print("\n key = ");
-    
-    key = textIO.next();
-    
-    List.remove(key);
-    
-    break;
-    
-    case 5:
-    
-    List.dump();
-    
-    break;
-    
-    case 6:
-    System.out.print("\n key to Update = ");
-    key = textIO.next();
-    System.out.print("\n Enter the New value  = ");
-    value = textIO.next();
-    List.update(key,value);
-    break;
-    case 7:
-    return;
-    
-    default:
-    
-    System.out.println(" Illegal command.");
-    
-    break;
-    
-    }
-    
-    System.out.println("\nHash List size is " + List.size());
-    
-    }
-}
+            Object key, value;
+
+            while (true) {
+
+              
+
+                System.out.print("Enter your command: ");
+
+                switch (textIO.nextInt()) {
+
+                    case 1:
+
+                        System.out.print("\n key = ");
+
+                        key = textIO.next();
+
+                        System.out.print(" value = ");
+
+                        value = textIO.next();
+
+                        List.put(key, value);
+
+                        break;
+
+                    case 2:
+
+                        System.out.print("\n key = ");
+
+                        key = textIO.next();
+
+                        System.out.println("The value at key " + key + "   is " + List.get(key));
+
+                        break;
+
+                    case 3:
+
+                        System.out.print("\n key = ");
+
+                        key = textIO.next();
+
+                        System.out.println(" containsKey(" + key + ") is " + List.containsKey(key));
+
+                        break;
+
+                    case 4:
+
+                        System.out.print("\n key = ");
+
+                        key = textIO.next();
+
+                        List.remove(key);
+
+                        break;
+
+                    case 5:
+
+                        List.dump();
+
+                        break;
+
+                    case 6:
+                        System.out.print("\n key to Update = ");
+                        key = textIO.next();
+                        System.out.print("\n Enter the New value  = ");
+                        value = textIO.next();
+                        List.update(key, value);
+                        break;
+                    case 7:
+                        return;
+
+                    default:
+
+                        System.out.println(" Illegal command.");
+
+                        break;
+
+                }
+
+                System.out.println("\nHash List size is " + List.size());
+
+            }
+        }
     }
 }
